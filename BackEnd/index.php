a2d5b37e50b87a0ebd0a3d1cb03fb9a02d3c0fa4
Ungureanu-Ioana <ioana000000@yahoo.com>
2018-05-24 13:45:51 +0300

    Add files via upload

---
+++ BackEnd/index.php
@@ -0,0 +1,3 @@
+<?php
+header( 'Location: http://www.autographcoll.com/Proiect/Proiect.html' );
+?>
---
+++ BackEnd/login.php
@@ -0,0 +1,29 @@
+<?php
+	include("config.php");
+	session_start();
+
+
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
+  	$email = $_POST['user_email'];
+  	$pwd = $_POST['password'];
+
+  	$query = "SELECT * FROM Users WHERE Email ='$email'";
+
+	$result = mysqli_query($conn,$query);
+	if (mysqli_num_rows($result) >= 1) {
+		$row = mysqli_fetch_assoc($result);
+
+		if($pwd != $row['Password']) {
+			echo "Wrong password.";
+			ob_end_flush();
+			flush();
+			sleep(3);
+			echo '<script language="javascript"> history.go(-1); </script>';
+		} else {
+		header( 'Location: http://www.autographcoll.com/FrontEnd/Home.html' );
+		}
+	}
+?>
