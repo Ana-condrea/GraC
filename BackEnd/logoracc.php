225eb78e795b5a9e0e793890c961c2f3ec7f7b2d
Ungureanu-Ioana <ioana000000@yahoo.com>
2018-05-28 11:26:42 +0300

    Update logoracc.php

--- BackEnd/logoracc.php
+++ BackEnd/logoracc.php
@@ -1,5 +1,6 @@
 <?php
-	if(PHP_SESSION_ACTIVE){
+	session_start();
+	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 		echo '<a href="account.html" class="right">Your Account</a>';
 	} else {
 		echo '<a href="login.html" class="right">Login</a>';
